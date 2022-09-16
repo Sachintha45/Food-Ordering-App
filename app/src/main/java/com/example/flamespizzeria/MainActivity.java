8e7e0b225690c043a5e49484727c4f296bbfdbd5
Sachintha45 <sachinthasenarathna10@gmail.com>
2022-09-16 13:18:44 +0530

    1.0

--- app/src/main/java/com/example/flamespizzeria/MainActivity.java
+++ app/src/main/java/com/example/flamespizzeria/MainActivity.java
@@ -26,51 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize Firebase Auth
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = auth.getCurrentUser();
-        if (currentUser == null) {
-            Intent intent = new Intent(this, Login.class);
-            startActivity(intent);
-            finish();
-            return;
-        }
 
-        tvFirstName = findViewById(R.id.tvFirstName);
-        tvPhone1 = findViewById(R.id.Phone1);
-        tvEmail = findViewById(R.id.tvEmail);
-
-        Button btnLogout = findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logoutUser();
-            }
-        });
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.getValue(User.class);
-                if (user != null) {
-                    tvFirstName.setText("First Name: " + user.firstName);
-                    tvPhone1.setText("Last Name: " + user.phone);
-                    tvEmail.setText("Email: " + user.email);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
-    }
-
-    private void logoutUser() {
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this, Login.class);
-        startActivity(intent);
-        finish();
     }
 }
